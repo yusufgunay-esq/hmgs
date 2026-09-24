5faa5e12c656ca1c9b977619fe84ae32c5c636c7
Yusuf Günay <yusufgunay@gmail.com>
2026-09-24 23:35:29 +0300

    yayin 2026-09-24 23:35

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Bu dosya HMGS projesinin tescilli kaynak kodudur. İzinsiz kopyalama, türetme
    veya yeniden yayınlama yasaktır. Lisans: depo kökündeki LICENSE dosyası. */
 
-const VERSION = 'hmgs-v72';
+const VERSION = 'hmgs-v73';
 const SHELL = VERSION + '-shell';
 
 // Uygulamanın kendi dosyaları + dışarıdan gelen görünüm dosyaları + Stüdyo kabuğu.
